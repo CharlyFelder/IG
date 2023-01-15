d55cb8ca7bab2e16e1c3e04b8d0cb20919baf1a7
OliHerz <olivier.herzog1997@gmail.com>
2023-01-15 15:42:41 +0100

    Full responsive design
    Tablet + Mobile version done.
    Mobile menu, Script JS, Fonts-sizes, Grid
    Page Done.

--- script/script_infos.js
+++ script/script_infos.js
@@ -1,13 +1,7 @@
-// Get the nav button and the nav links
-const navButton = document.querySelector(".navButton");
-const navLinks = document.querySelector(".navLinks");
+const hamburger = document.querySelector('.hamburger');
+const navBar = document.querySelector('.navBar');
 
-// querySelector = calss/attribu
-// GetEelemnt = tableau 
-
-// Add a click event listener to the nav button
-navButton.addEventListener('click', function() {
-  // Toggle the "open" class on the nav links
-  navLinks.classList.add('open');
-//   navButton.classList.add('open');
-})
+hamburger.addEventListener('click', function (){
+  hamburger.classList.toggle('is-active');
+  navBar.classList.toggle('navBar-active');
+});
